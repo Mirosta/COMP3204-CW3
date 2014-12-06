3b7b86a26135449af65324bb5c7433d902e9ea75
Thomas Williams <t.r.williamst@googlemail.com>
2014-12-06 19:05:24 +0000

    Irrelevant changes

--- src/main/java/uk/ac/soton/ecs/twtk/LinearClassifier.java
+++ src/main/java/uk/ac/soton/ecs/twtk/LinearClassifier.java
@@ -1,7 +1,16 @@
 package uk.ac.soton.ecs.twtk;
 
 import org.openimaj.data.dataset.Dataset;
+import org.openimaj.data.dataset.GroupedDataset;
+import org.openimaj.data.dataset.ListDataset;
+import org.openimaj.experiment.evaluation.classification.ClassificationResult;
 import org.openimaj.image.FImage;
+import org.openimaj.ml.annotation.linear.LiblinearAnnotator;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Created by Tom on 02/12/2014.
@@ -15,14 +24,28 @@ public class LinearClassifier implements TestableClassifier
     }
 
     @Override
-    public void train(Dataset<FImage> trainingSet)
+    public void train(GroupedDataset<String, ? extends ListDataset<FImage>, FImage> trainingSet)
     {
-
     }
 
     @Override
-    public String classify(FImage image)
+    public ClassificationResult<String> classify(FImage image)
     {
-        return "Test";
+        return new ClassificationResult<String>()
+        {
+            @Override
+            public double getConfidence(String s)
+            {
+                return 0;
+            }
+
+            @Override
+            public Set<String> getPredictedClasses()
+            {
+                List<String> list = new ArrayList<String>();
+                list.add("Test");
+                return new HashSet<String>(list);
+            }
+        };
     }
 }
