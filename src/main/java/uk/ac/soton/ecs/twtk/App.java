b5afaae9aeff399fd9aa34f039ed2c5bcfd2a199
Trisha Khallaghi <tk7g11@soton.ac.uk>
2014-12-06 20:42:42 +0000

    added code to average runs over several values of k

--- src/main/java/uk/ac/soton/ecs/twtk/App.java
+++ src/main/java/uk/ac/soton/ecs/twtk/App.java
@@ -1,18 +1,41 @@
 package uk.ac.soton.ecs.twtk;
 
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 
-/**
- * OpenIMAJ Hello world!
- *
- */
+
 public class App
 {
     public static void main( String[] args ) throws IOException
     {
-        TestHarness run1Harness = new TestHarness(new KNNClassifier(), "run1.txt");
-        run1Harness.testRun(20, 20);
-        //TestHarness run2Harness = new TestHarness(new LinearClassifier(), "run2.txt");
-        //run2Harness.run();
+    	int N = 25;
+    	int noIterations = 5;
+    	
+        FileOutputStream outputStream = new FileOutputStream("test-results.csv");
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream));
+        
+        writer.write("run,k-value,no_iterations,averaged ratio");
+        writer.newLine();
+        
+        for (int i=10; i<20; i++) 
+        {
+        	System.out.println("Run: " + (i-9) + ", K-value: " + i);
+        	TestHarness runHarness = new TestHarness(new KNNClassifier(i), "run" + i + ".txt", true);
+        	double totalRatio = 0;
+        	for (int n=0; n<noIterations; n++) 
+        	{
+        		totalRatio += runHarness.testRun(N, N);
+        	}
+        	System.out.println("Averaged ratio (" + noIterations + " runs): " + totalRatio/noIterations + "\n\n");
+        	
+        	String csvString = (i-9) + "," + i + "," + noIterations + "," + totalRatio/noIterations;
+        	writer.write(csvString);
+        	writer.newLine();
+        }
+        
+        writer.close();
+        
     }
 }
